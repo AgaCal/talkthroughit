8a00c39769d7bda1e3798d8d52cd98064e7d2c60
aga <calkowska.aga@gmail.com>
2025-10-26 00:06:46 -0400

    fixed wiping image buggit

--- src/talkthroughit/components/whiteboard.py
+++ src/talkthroughit/components/whiteboard.py
@@ -2,7 +2,7 @@ import streamlit as st
 from math import ceil
 from streamlit_drawable_canvas import st_canvas
 from streamlit_option_menu import option_menu
-from .color_selector import color_select
+from talkthroughit.components.color_selector import color_select
 from PIL import Image
 from io import BytesIO
 import base64
@@ -21,10 +21,10 @@ def whiteboard(width = 500, height = 500):
     make sure that the entire canvas fits in the screen!!! otherwise there's some weird behavior that's not worth fixing right now
     '''
     # to display canvas
-    canvas_placeholder = st.empty()
+    # canvas_placeholder = st.empty()
     col1, col2 = st.columns(2)
 
-    with col2:
+    with col1:
         def select_drawing_mode():
             drawing_modes = {
                 "freedraw": "pencil",
@@ -49,7 +49,7 @@ def whiteboard(width = 500, height = 500):
         
         drawing_mode = select_drawing_mode()
 
-    with col1:
+    with col2:
 
         label_col, slider_col = st.columns([1,3], vertical_alignment="center")
 
@@ -62,7 +62,7 @@ def whiteboard(width = 500, height = 500):
             stroke_opacity = st.slider("Opacity", label_visibility="collapsed", min_value=0.0, max_value=1.0, value=1.0)
         fill = st.checkbox(label = "Fill?", value=True) if drawing_mode in ["rect", "circle"] else False 
 
-    with col2:
+    with col1:
         _, tmp_col_2, _ = st.columns([1,10,1])
 
         def select_stoke_color():
@@ -72,18 +72,18 @@ def whiteboard(width = 500, height = 500):
         with (tmp_col_2):
             stroke_color = select_stoke_color()
 
-    with canvas_placeholder.container():
-        canvas_result = st_canvas(
-            stroke_width=stroke_width,
-            stroke_color=stroke_color if not fill else TRANSPARENT,
-            fill_color= stroke_color if fill else TRANSPARENT,
-            background_color=WHITE,
-            update_streamlit=True,
-            width=width,
-            height=height,
-            drawing_mode=drawing_mode,
-            key="canvas",
-        )
+    canvas_result = st_canvas(
+        stroke_width=stroke_width,
+        stroke_color=stroke_color if not fill else TRANSPARENT,
+        fill_color= stroke_color if fill else TRANSPARENT,
+        background_color=WHITE,
+        update_streamlit=True,
+        width=width,
+        height=height,
+        drawing_mode=drawing_mode,
+        key="canvas",
+    )
+
 
     if canvas_result.image_data is not None:
         img = Image.fromarray((canvas_result.image_data).astype('uint8'), 'RGBA')
@@ -95,4 +95,5 @@ def whiteboard(width = 500, height = 500):
         st.session_state.current_tab_data["content"] = img_str
 
 if __name__ == "__main__":
+    st.session_state.current_tab_data = dict()
     whiteboard(width=750)
