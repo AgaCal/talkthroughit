3136e1b8cf13fc394904ec0edef1c2b92846a3f8
aga <calkowska.aga@gmail.com>
2025-10-26 08:30:43 -0400

    more whiteboard changes

--- src/talkthroughit/components/whiteboard.py
+++ src/talkthroughit/components/whiteboard.py
@@ -14,20 +14,17 @@ def whiteboard(width = 500, height = 500):
     '''
     a whiteboard component using streamlit-drawable-canvas
     doesn't return anything, but saves the current image as a base64 string in st.session_state.current_tab_data["content"]
-    buggy and resets on some interactions, will probably be fixed once i save stuff in session state somehow
-
     width and height are the dimensions of the canvas - not the entire component! menu extends beyond that
-
     make sure that the entire canvas fits in the screen!!! otherwise there's some weird behavior that's not worth fixing right now
     '''
-    # to display canvas
     
     whiteboard_container = st.container()
 
     with whiteboard_container:
-        col1, col2 = st.columns([5,3])
+        col1, col2 = st.columns([5,3], vertical_alignment='top')
 
     with col1:
+
         def select_drawing_mode():
             drawing_modes = {
                 "freedraw": "pencil",
@@ -55,24 +52,19 @@ def whiteboard(width = 500, height = 500):
         drawing_mode = select_drawing_mode()
 
     with col2:
-
-        width_col, opacity_col = st.columns([1,1], vertical_alignment="center")
+        width_col, opacity_col = st.columns(2)
 
         with width_col:
-            st.markdown("Width:")
-            stroke_width = st.slider(label="Width", label_visibility="collapsed", min_value=1, max_value=25, value=3)
+            stroke_width = st.slider(label="Width", min_value=1, max_value=25, value=3)
 
         with opacity_col:
-            st.markdown("Opacity:")
-            stroke_opacity = st.slider("Opacity", label_visibility="collapsed", min_value=0.0, max_value=1.0, value=1.0)
+            stroke_opacity = st.slider("Opacity", min_value=0.0, max_value=1.0, value=1.0)
         
-        fill_container = st.container()
-
         if drawing_mode in ["rect", "circle"]:
-            fill = fill_container.checkbox(label = "Fill?", value=True)
+            fill = st.checkbox(label = "Fill?", value=True)
         else:
             fill = False
-            fill_container.markdown('<div style="height: 48px;">&nbsp;</div>', unsafe_allow_html=True)
+            st.markdown('<div style="height: 40px;">&nbsp;</div>', unsafe_allow_html=True)
 
     with col1:
         def select_stoke_color():
@@ -80,7 +72,6 @@ def whiteboard(width = 500, height = 500):
             return tmp_stroke_color, tmp_stroke_color + (hex(ceil(stroke_opacity * 255))[2:]).zfill(2)
 
         tmp_stroke_color, stroke_color = select_stoke_color()
-
 
     with whiteboard_container:
         canvas_result = st_canvas(
